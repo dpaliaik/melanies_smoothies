3e7ea8831cb8bda8df5f7409503cc473de651aff
Dimitrios Paliaikos <50620845+dpaliaik@users.noreply.github.com>
2025-01-26 13:21:18 +0000

    Present data from an API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -41,3 +42,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered ' + name_on_order + '!', icon="✅")
 
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    #st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
